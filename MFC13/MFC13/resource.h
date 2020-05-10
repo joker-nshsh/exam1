40d3242ebf97cfd6756fa0cb9ba5adf557104579
joker-nshsh <1569849722@qq.com>
2020-05-10 18:11:46 +0800

    资源：画圆、直线、矩形

---
+++ MFC13/MFC13/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFC13.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_MFC13TYPE                   130
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define juxing                          32774
+#define yuan                            32775
+#define zhixian                         32776
+#define ID_juxing                       32777
+#define ID_yuan                         32778
+#define ID_zhixian                      32779
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
